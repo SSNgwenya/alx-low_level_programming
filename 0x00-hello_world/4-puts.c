08bfa24ec352589626c91cc91b1f58c3123556f9
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-11 15:40:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - this function is called a main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+
+}
