f7849683604c68b49a8058f5be37761c169399b9
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-17 13:38:04 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is a main function
+ * Return: Always return 0 when successful
+ */
+
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
