197f3b4ca8fff6dfccb2037d16ce2bc7b2f18e0c
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-17 14:34:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - This a main function
+ * Return: Always return 0 when successful
+ */
+
+int main(void)
+{
+	char num,
+
+	     int eber;
+
+	num = 'a';
+	eber = 0;
+	while
+		(eber < 10) {
+			putchar(eber + '0');
+			eber++;
+		}
+	while 
+		(num <= 'f') {
+			putchar(num);
+			num++;
+		}
+	putchar('\n');
+	return (0);
+}
