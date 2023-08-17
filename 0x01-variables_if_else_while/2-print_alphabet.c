74cb7eba50d51fe6c3fe615e014a89a6ed921b14
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-17 13:09:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - This is a main function
+ * Return: Always return 0 if successful
+ */
+
+int main(void)
+{
+	char c;
+	c = "a";
+	while
+		(c <= "z") {putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
