95a02f770879f6285f3223d4e419325626f66958
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-17 14:25:03 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This is a main function
+ * Return: Always return 0 when successful
+ */
+
+int main()
+{
+	char l;
+
+	l = 'z';
+	while
+		(l >= 'a') {
+			putchar(l);
+			l--;
+		}
+	putchar('\n');
+	return (0);
+}
