7f34241e2bf0527dbe986a9c1630a4b0138b93af
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-17 20:09:46 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,17 @@
 
 void more_numbers(void)
 {
-	int q, w;
+	int q;
+	
+	int w;
 
 	for (q = 0; q < 10; q++)
 	{
 		for (w = 0; w < 15; w++)
 		{
 			if (w >= 10)
-			_putchar((w / 10) + '0');
-			_putchar((w % 10) + '0');
+			_putchar(w / 10 + '0');
+			_putchar(w % 10 + '0');
 		}
 	}
 	_putchar('\n');
