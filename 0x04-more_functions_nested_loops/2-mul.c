fa974ed77a7110f157597f9f7389366454025448
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-17 19:29:45 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - functions checks numbers
+ * @a: variable
+ * @b: variable
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
