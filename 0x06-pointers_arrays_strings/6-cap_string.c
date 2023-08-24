2d9435e6778b73b66f0ee1e1fd8e5af297ff05cc
SSNgwenya <sunnyngwenya@gmail.com>
2023-08-24 16:54:40 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * cap_string - cap all each charecter
+ * cap_string - capitalizes each word
  * @s: string being capitalized
  * Return: capitalized string
  */
 
 char *cap_string(char *s)
 {
-	int q;
-	int m;
-	char ss[13] = {' ', '\t', '\n', ',',
-';', ',', '!', '?', ' '' ', '(', ')', '{', '}'};
+	int i,j;
+	char sp[13] = {' ', '\t', '\n', ',',
+':', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	for (q = 0; s[q] != '\0'; q++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (q == 0 && s[q] >= 'a' && s[q] <= 'z')
-			s[q] = s[q] - 32;
-		for (m = 0; m < 13; m++)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		for (j = 0; j < 13; j++)
 		{
-			if (s[q] == ss[m])
+			if (s[i] == sp[j])
 			{
-				if (s[q + 1] >= 'a' && s[q + 1] <= 'z')
-					s[q + 1] = s[q + 1] - 32;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - 32;
 			}
 		}
 	}
