847567ac28952bb96e7fadecfe17081c7fc55684
SSNgwenya <sunnyngwenya@gmail.com>
2023-10-26 10:07:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - recursive function to print to the screen
  * @s: pointer to the string
@@ -7,7 +8,7 @@
 
 void _puts_recursion(char *s)
 {
-	if (*S != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
