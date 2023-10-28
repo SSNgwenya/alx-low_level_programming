9bdc0ec3bfc7a3a5c105877d423998f7920fbcdd
SSNgwenya <sunnyngwenya@gmail.com>
2023-10-27 18:24:11 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function thats adds two numbers
+ * @sunny: parameter 1
+ * @m: parameter 2
+ * Return: sunny + m
+ */
+
+int add(int sunny, int m)
+{
+	return (sunny + m);
+}
