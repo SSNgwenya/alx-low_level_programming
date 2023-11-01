a49c540f27bf2014851326361dedf9bea6332398
SSNgwenya <sunnyngwenya@gmail.com>
2023-11-01 06:35:35 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ * main - generates random passwords for 101-crackme program
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int password[100];
+	int k;
+	int s = 0;
+	int a;
+
+	srand(time(NULL));
+	for (k = 0; k < 100; k++)
+	{
+		password[k] = rand() % 78;
+		s += (password[k] + '0');
+		putchar(password[k] + '0');
+		if ((2772 - s) - '0' < 78)
+		{
+			a = 2772 - s - '0';
+			s += a;
+			putchar(a + '0');
+			break;
+		}
+	}
+	return (0);
+}
