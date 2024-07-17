ba7bf3e75cb84dbf669bde1600891060f696f858
SSNgwenya <sunnyngwenya@gmail.com>
2024-07-17 16:05:24 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,32 +5,24 @@ This module calculates the perimeter of an island described in a grid.
 
 def island_perimeter(grid):
     """
-    Calculate the perimeter of the island described in grid.
-    
-    Args:
-        grid (list of list of int): A list of lists where 0 represents a water zone and 1 represents a land zone.
-        
-    Returns:
-        int: The perimeter of the island.
+   
+   Returns perimeter of the island
+
+    grid represnts water zone by zero and land by one
+
     """
-    columns = len(grid[0])
-    perimeter = 0
-    rows = len(grid)
 
-    a = 0
-    while a < rows:
-        d = 0
-        while d < columns:
-            if grid[a][d] == 1:
-                if a == 0 or grid[a - 1][d] == 0:
-                    perimeter += 1
-                if a == rows - 1 or grid[a + 1][d] == 0:
-                    perimeter += 1
-                if d == 0 or grid[a][d - 1] == 0:
-                    perimeter += 1
-                if d == columns - 1 or grid[a][d + 1] == 0:
-                    perimeter += 1
-            d += 1
-        a += 1
+    height = len(grid)
+    width = len(grid[0])
+    sides = 0
+    size = 0
 
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    sides += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    sides += 1
+    return size * 4 - sides * 2
